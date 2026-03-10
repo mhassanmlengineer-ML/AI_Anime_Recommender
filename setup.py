878c3b85279b927a2964da1d5affba41aef7b8bd
mhassanmlengineer-ML <mhassan.mlengineer@gmail.com>
2026-03-10 14:15:16 +0300

    Ignore virtual environment

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name = "MLOPS",
-    version = "1.0.0",
+    name = "AI_ANIME_RECOMMENDER",
+    version = "0.1",
     author = "Mohamed Hassan",
     packages = find_packages(),
     install_requires = requirements
